b690a0a8a6b0d5f218a088570a49b2d10fbd0bde
Dmitrii Sumenko <64180264+flstudio4@users.noreply.github.com>
2023-10-15 01:06:45 +0000

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,8 @@
 #  index_users_on_username              (username) UNIQUE
 #
 class User < ApplicationRecord
+
+  validates :username, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
